7336e278441b672f98b2292054474a1f25ab32e5
shikharbahikmagar <shikharbahik5@gmail.com>
2023-11-24 09:25:42 +0545

    css changes

--- practices/05passwordGenerator/src/App.jsx
+++ practices/05passwordGenerator/src/App.jsx
@@ -47,7 +47,7 @@ function App() {
   return (
     <>
       <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 py-4 text-orange-500 bg-gray-700'>
-        <h1 className='text-white text-center my-5'>Password Generator</h1>
+        <h1 className='text-white hover:text-sky-400 text-center my-5'>Password Generator</h1>
         <div className='flex shadow rounded-lg overflow-hidden mb-4'>
             <input type="text" value={password} className='outline-none w-full py-1 px-3'
             placeholder='Password'
